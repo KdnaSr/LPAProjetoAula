e337642a516f49affc03ae9d7121ebc79086d6de
KelvinKdna <kelvinn.donascimento22@gmail.com>
2024-06-23 01:09:01 -0300

    Text Menu

--- Diagrama_Classe_Jogo/Game.py
+++ Diagrama_Classe_Jogo/Game.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-class Game:
-    def __init__(self):
-        self.Window = None
+import pygame as pygame
 
-    def run(self, ):
-        pass
+from Menu import Menu
+
+
+class Game:
+
+    def __init__(self):
+        pygame.int()
+        self.window = pygame.display.setmode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            
+            pass
 
--- Diagrama_Classe_Jogo/Menu.py
+++ Diagrama_Classe_Jogo/Menu.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import pygame
+
+
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.init.load()
 
     def run(self, ):
         pass
